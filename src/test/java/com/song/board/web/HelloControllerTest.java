35a67ab0532536de21a91bfaa7d7714f18732cc8
songhee27 <songhee27@ajpark.co.kr>
2021-05-17 21:31:42 +0900

    test코드.. 에러 해결중

--- src/test/java/com/song/board/web/HelloControllerTest.java
+++ src/test/java/com/song/board/web/HelloControllerTest.java
@@ -1,14 +1,12 @@
 package com.song.board.web;
 
-
-import com.song.board.web.HelloController;
-import com.song.board.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,14 +20,6 @@ public class HelloControllerTest {
 
     @Test
     public void hello가_리턴된다() throws Exception {
-        // given
-        String name = "test";
-        int amount = 1000;
-
-        // when
-        // HelloResponseDto dto = new HelloResponseDto(name, amount);
-
-        //
         String hello = "hello";
 
         mvc.perform(get("/hello"))
